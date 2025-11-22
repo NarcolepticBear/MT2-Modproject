cf97916910149f725d940c651440d7dc44cbfd73
NarcolepticBear <113416991+NarcolepticBear@users.noreply.github.com>
2025-11-22 16:15:08 +0000

    some more card changes. shadowsiege changes disabled

--- a_balance_mod.Plugin/Plugin.cs
+++ a_balance_mod.Plugin/Plugin.cs
@@ -23,11 +23,13 @@ namespace a_balance_mod.Plugin
                 {
                     // Be sure to include any new json files if you add more.
                     c.AddMergedJsonFile(
-                        "json/plugin.json",
                         "json/global.json",
-                        "json/steelenhancer.json",
-                        "json/greeddragon.json",
-                        "json/monstergreeddragon.json"
+                        "json/units/greeddragon.json",
+                        "json/units/monstermorselmaster.json",
+                        "json/spells/hallowedhalls.json",
+                        "json/spells/hoarfrosteffigy.json",
+                        "json/units/shadowsiege.json",
+                        //"json/units/monstershadowsiege.json"
                     );
                 }
             );
